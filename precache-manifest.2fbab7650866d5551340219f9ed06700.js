8bf261fb5b137f21671f2d988ca27784724cd0e4
brknd <b.guener@brknd.xyz>
2020-04-26 13:35:18 +0200

    .

---
+++ precache-manifest.2fbab7650866d5551340219f9ed06700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9b3c098236a15bf227e36f817a7053",
+    "url": "/brknd.github.io/index.html"
+  },
+  {
+    "revision": "29e4c72c120ffd3418c8",
+    "url": "/brknd.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d06e91e4b631dfc7e85d",
+    "url": "/brknd.github.io/static/js/2.7a2b8648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brknd.github.io/static/js/2.7a2b8648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e4c72c120ffd3418c8",
+    "url": "/brknd.github.io/static/js/main.4afe10f8.chunk.js"
+  },
+  {
+    "revision": "3d54c0ebd54ea8e60cef",
+    "url": "/brknd.github.io/static/js/runtime-main.f3e81aa4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brknd.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
